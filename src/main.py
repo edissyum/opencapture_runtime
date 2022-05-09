b71259519c407e0650d0854c1aa0b0b5d478cf74
Nathan <nathan.cheval@outlook.fr>
2022-05-09 11:21:40 +0200

    Update

--- src/main.py
+++ src/main.py
@@ -460,7 +460,7 @@ if __name__ == '__main__':
     cpt = 1
     number_of_prescription = len(os.listdir(prescription_path))
     for prescription in os.listdir(prescription_path):
-        if os.path.splitext(prescription)[1] == '.jpg' and prescription == 'ordo.jpg':
+        if os.path.splitext(prescription)[1] == '.jpg':  # and prescription == 'ordo.jpg':
             start = time.time()
             # Set up data about the prescription
             file = prescription_path + prescription
